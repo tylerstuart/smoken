2e5bb313b0f848967f475179eaed1fab2499c9c4
Tyler Stuart <tstuart@1on1.com>
2012-06-12 11:13:46 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smoken::Application.config.secret_token = '0b21b6b2206f1039170717e9b77002170b67c34493155c8456498760b64917655558d42cb06c609d21014e114f1f834b57283047d8cc056e7dfbcc32ad71581b'
