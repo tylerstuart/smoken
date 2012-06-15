887b65254c73720262784f509d79a740576e6efe
Tyler Stuart <tstuart@1on1.com>
2012-06-15 10:07:44 -0600

    Finished sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Smoken::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
-  match '/signup',  :to =>'users#new'
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
-  match '/help',   :to => 'static_pages#help'
-  match '/about',  :to => 'static_pages#about'
-  root       :to => 'static_pages#home'
+  match '/help',   to:  'static_pages#help'
+  match '/about',  to:  'static_pages#about'
+  root       to:  'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
